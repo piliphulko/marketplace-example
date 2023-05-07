1db574cff5c4b8ade917204ff180ec8e225a5b76
pilip <piliphulko25@gmail.com>
2023-05-07 22:27:01 +0300

    update html marketplace

--- internal/pkg/httpout/handler.go
+++ internal/pkg/httpout/handler.go
@@ -20,6 +20,15 @@ type Goods struct {
 	AmountGoods   string
 }
 
+type Order struct {
+	NameWarehouse string
+	NameVendor    string
+	NameGoods     string
+	AmountGoods   float64
+	PriceGoods    float64
+	Totalcost     float64
+}
+
 func HandlerCustomerCreatePage(w http.ResponseWriter, r *http.Request) {
 	html, err := template.ParseFiles("../../html/customer-create.html")
 	if err != nil {
@@ -75,28 +84,46 @@ func HandlerMarketplacePage(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	if html.Execute(w, struct{ GoodsARRAY []Goods }{
-		GoodsARRAY: []Goods{
-			{
-				NameWarehouse: "1",
-				NameVendor:    "1",
-				TypeGoods:     "1",
-				NameGoods:     "1",
-				InfoGoods:     "1",
-				PriceGoods:    "1",
-				AmountGoods:   "1",
+	if html.Execute(w,
+		struct {
+			GoodsARRAY  []Goods
+			BoolOrders  bool
+			OrderUuid   string
+			OrdersARRAY []Order
+		}{
+			GoodsARRAY: []Goods{
+				{
+					NameWarehouse: "1",
+					NameVendor:    "1",
+					TypeGoods:     "1",
+					NameGoods:     "1",
+					InfoGoods:     "1",
+					PriceGoods:    "1",
+					AmountGoods:   "1",
+				},
+				{
+					NameWarehouse: "2",
+					NameVendor:    "2",
+					TypeGoods:     "2",
+					NameGoods:     "2",
+					InfoGoods:     "2",
+					PriceGoods:    "2",
+					AmountGoods:   "2",
+				},
 			},
-			{
-				NameWarehouse: "2",
-				NameVendor:    "2",
-				TypeGoods:     "2",
-				NameGoods:     "2",
-				InfoGoods:     "2",
-				PriceGoods:    "2",
-				AmountGoods:   "2",
+			BoolOrders: false,
+			OrderUuid:  "qscdsad",
+			OrdersARRAY: []Order{
+				{
+					NameWarehouse: "",
+					NameVendor:    "",
+					NameGoods:     "",
+					AmountGoods:   1,
+					PriceGoods:    1,
+					Totalcost:     1,
+				},
 			},
-		},
-	}) != nil {
+		}) != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
