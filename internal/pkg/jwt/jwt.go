a2bfd942c612d6216782b3c629d68d8ad4b36bec
pilip <piliphulko25@gmail.com>
2023-06-08 00:49:57 +0300

    basic JWT functionality is ready

--- internal/pkg/jwt/jwt.go
+++ internal/pkg/jwt/jwt.go
@@ -6,6 +6,7 @@ import (
 	"crypto/sha256"
 	"encoding/base64"
 	"errors"
+	"time"
 
 	jsoniter "github.com/json-iterator/go"
 )
@@ -17,10 +18,12 @@ var (
 )
 
 var (
-	ErrFakeToken = errors.New("fake token")
+	ErrTokenFake    = errors.New("token fake")
+	ErrTokenExpired = errors.New("token expired")
 )
 
-func InsertSecretForSignJWT(secret string) {
+// InsertSecretForSignJWT inserts the key to sign the JWS
+func InsertSecretForSignJWS(secret string) {
 	keySecret = []byte(secret)
 }
 
@@ -31,7 +34,7 @@ type Header struct {
 
 type Payload struct {
 	Nickname string `json:"nickname"`
-	Exp      string `json:"exp"`
+	Exp      int64  `json:"exp"`
 }
 
 type JWS []byte
@@ -44,51 +47,45 @@ func (jwt JWT) String() string {
 	return string(jwt.body)
 }
 
+// CreateJWS creates a JWS to be signed
 func CreateJWS(h Header, p Payload) (JWS, error) {
-	var (
-		bufJWS           = bytes.Buffer{}
-		encoderBase64URL = base64.NewEncoder(base64.URLEncoding, &bufJWS)
-		jsonEncoder      = JSON.NewEncoder(encoderBase64URL)
-	)
-	defer encoderBase64URL.Close()
 
-	if err := jsonEncoder.Encode(&h); err != nil {
+	hJson, err := JSON.Marshal(h)
+	if err != nil {
 		return nil, err
 	}
-	bufJWS.WriteByte(46) // Write "."
-
-	if err := jsonEncoder.Encode(&p); err != nil {
+	pJson, err := JSON.Marshal(p)
+	if err != nil {
 		return nil, err
 	}
-
-	return bufJWS.Bytes(), nil
+	hJsonBase64 := make([]byte, base64.RawURLEncoding.EncodedLen(len(hJson)))
+	pJsonBase64 := make([]byte, base64.RawURLEncoding.EncodedLen(len(pJson)))
+	base64.RawURLEncoding.Encode(hJsonBase64, hJson)
+	base64.RawURLEncoding.Encode(pJsonBase64, pJson)
+	return append(append(hJsonBase64, byte(46)), pJsonBase64...), nil
 }
 
+// SignJWS signs a JWS creating a JWT
 func (jws JWS) SignJWS() (JWT, error) {
 	var (
-		bufJWT           = bytes.Buffer{}
-		encoderBase64URL = base64.NewEncoder(base64.URLEncoding, &bufJWT)
+		bufJWT = bytes.Buffer{}
+		mac    = hmac.New(sha256.New, keySecret)
 	)
-	defer encoderBase64URL.Close()
-
-	mac := hmac.New(sha256.New, keySecret)
 
 	if _, err := mac.Write(jws); err != nil {
 		return JWT{nil}, err
 	}
-	/*
-		signature, err := blockAes.AesEncodeBytesGCM(jws)
-		if err != nil {
-			return "", nil
-		}
-	*/
 	if _, err := bufJWT.Write(jws); err != nil {
 		return JWT{nil}, err
 	}
 	if err := bufJWT.WriteByte(46); err != nil { // Write "."
 		return JWT{nil}, err
 	}
-	if _, err := encoderBase64URL.Write(mac.Sum(nil)); err != nil {
+	Signature := mac.Sum(nil)
+	SignatureBase64URL := make([]byte, base64.RawURLEncoding.EncodedLen(len(Signature)))
+	base64.RawURLEncoding.Encode(SignatureBase64URL, Signature)
+
+	if _, err := bufJWT.Write(SignatureBase64URL); err != nil {
 		return JWT{nil}, err
 	}
 	return JWT{bufJWT.Bytes()}, nil
@@ -102,27 +99,71 @@ func (jwt JWT) splitHeaderPayloadSignature() ([]byte, []byte, []byte) {
 	return header, payload, signature
 }
 
+// CheckJWT checks the integrity and authenticity of the token, as well as the expiration of the token
+// possible errors: ErrTokenFake, ErrTokenExpired, others standart errors
 func (jwt JWT) CheckJWT() error {
 	var (
-		mac              = hmac.New(sha256.New, keySecret)
-		bufHMAC          = bytes.Buffer{}
-		encoderBase64URL = base64.NewEncoder(base64.URLEncoding, &bufHMAC)
-
+		mac                        = hmac.New(sha256.New, keySecret)
 		header, payload, signature = jwt.splitHeaderPayloadSignature()
 		headerPayload              = append(append(header, byte(46)), payload...)
+
+		payloadJson = make([]byte, base64.RawURLEncoding.DecodedLen(len(payload)))
+		payloadType Payload
 	)
-	defer encoderBase64URL.Close()
 
 	if _, err := mac.Write(headerPayload); err != nil {
 		return err
 	}
+	signatureHMAC := mac.Sum(nil)
+	signatureHMACBase64URL := make([]byte, base64.RawURLEncoding.EncodedLen(len(signatureHMAC)))
+	base64.RawURLEncoding.Encode(signatureHMACBase64URL, signatureHMAC)
 
-	if _, err := encoderBase64URL.Write(mac.Sum(nil)); err != nil {
+	if !hmac.Equal(signatureHMACBase64URL, signature) {
+		return ErrTokenFake
+	}
+	base64.RawURLEncoding.Decode(payloadJson, payload)
+	if err := JSON.Unmarshal(payloadJson, &payloadType); err != nil {
 		return err
 	}
-
-	if !hmac.Equal(bufHMAC.Bytes(), signature) {
-		return ErrFakeToken
+	if time.Now().Unix() > payloadType.Exp {
+		return ErrTokenExpired
 	}
+
 	return nil
 }
+
+// TakeNickname returns nickname from token payload
+func (jwt JWT) TakeNickname() (string, error) {
+	var (
+		_, payload, _ = jwt.splitHeaderPayloadSignature()
+		payloadJson   = make([]byte, base64.RawURLEncoding.DecodedLen(len(payload)))
+		payloadType   Payload
+	)
+	base64.RawURLEncoding.Decode(payloadJson, payload)
+	if err := JSON.Unmarshal(payloadJson, &payloadType); err != nil {
+		return "", err
+	}
+	return payloadType.Nickname, nil
+}
+
+/*
+	func CreateJWS(h Header, p Payload) (JWS, error) {
+		var (
+			bufJWS           = bytes.Buffer{}
+			encoderBase64URL = base64.NewEncoder(base64.StdEncoding, &bufJWS)
+			jsonEncoder      = jsoniter.NewEncoder(encoderBase64URL)
+		)
+		defer encoderBase64URL.Close()
+
+		if err := jsonEncoder.Encode(&h); err != nil {
+			return nil, err
+		}
+
+		bufJWS.WriteByte(46) // Write "."
+
+		if err := jsonEncoder.Encode(&p); err != nil {
+			return nil, err
+		}
+		return bufJWS.Bytes(), nil
+	}
+*/
--- internal/pkg/jwt/jwt_test.go
+++ internal/pkg/jwt/jwt_test.go
@@ -2,23 +2,15 @@ package jwt
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
 
-/*
-func TestMain(m *testing.M) {
-
-		InsertSecretForSignJWT("12345678qwertyui")
-
-		testRun := m.Run()
-		os.Exit(testRun)
-	}
-*/
 func TestTotal(t *testing.T) {
-	InsertSecretForSignJWT("12345678qwertyui")
+	InsertSecretForSignJWS("12345678qwertyui")
 
-	jws, err := CreateJWS(Header{Alg: "AES", Typ: "JWT"}, Payload{Nickname: "test", Exp: "2422"})
+	jws, err := CreateJWS(Header{Alg: "AES", Typ: "JWT"}, Payload{Nickname: "test", Exp: time.Now().Add(1 * time.Hour).Unix()})
 	require.Nil(t, err)
 
 	jwt, err := jws.SignJWS()
@@ -27,7 +19,45 @@ func TestTotal(t *testing.T) {
 	err = jwt.CheckJWT()
 	require.Nil(t, err)
 
+	login, err := jwt.TakeNickname()
+	require.Nil(t, err)
+	require.Equal(t, "test", login)
+
 	jwt.body = []byte("eyJhbGciOiJBRVMiLCJ0eXAiOiJKV1Qi.fQp7Im5pY2tuYW1lIjoidGVzdCIsImV4cCI6IjI0MjIi.QQ8-1MOWCthrrOaMsF_S--yKKf-FYTDvx6RDGn")
 	err = jwt.CheckJWT()
-	require.ErrorIs(t, err, ErrFakeToken)
+	require.ErrorIs(t, err, ErrTokenFake)
+
+	jws, err = CreateJWS(Header{Alg: "AES", Typ: "JWT"}, Payload{Nickname: "test", Exp: time.Now().Unix() - 1})
+	require.Nil(t, err)
+
+	jwt, err = jws.SignJWS()
+	require.Nil(t, err)
+
+	err = jwt.CheckJWT()
+	require.ErrorIs(t, err, ErrTokenExpired)
 }
+
+var bendE *JWS
+
+func BenchmarkJWS(b *testing.B) {
+	InsertSecretForSignJWS("12345678qwertyui")
+	var jws JWS
+	var err error
+	for i := 0; i != 10000; i++ {
+		jws, err = CreateJWS(Header{Alg: "AES", Typ: "JWT"}, Payload{Nickname: "test", Exp: time.Now().Add(time.Minute).Unix()})
+		require.Nil(b, err)
+		jwt, err := jws.SignJWS()
+		require.Nil(b, err)
+		require.Nil(b, jwt.CheckJWT())
+	}
+	bendE = &jws
+}
+
+/*
++ CreateJWS
+BenchmarkJWS-8   	1000000000	         0.02022 ns/op	       0 B/op	       0 allocs/op
++ SignJWS
+BenchmarkJWS-8   	1000000000	         0.04697 ns/op	       0 B/op	       0 allocs/op
++ CheckJWT
+BenchmarkJWS-8   	1000000000	         0.08082 ns/op	       0 B/op	       0 allocs/op
+*/
